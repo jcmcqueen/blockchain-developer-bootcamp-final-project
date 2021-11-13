0b9c8eecd233929f734ad8bd1e1e514fdd93f9c4
Craig McQueen <craig_mcqueen@hotmail.com>
2021-11-13 15:27:22 -0500

    Created shell for contract data.

---
+++ contracts/healthData.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16 <0.9.0;
+
+    // User Profle. 
+    // 
+    mapping (address => uint) internal balances ;
+
+contract SupplyChain {
+
+    /* Functions
+     */
+     function createProfile () {
+
+     }
+
+     function storeData( ) {
+
+     }
